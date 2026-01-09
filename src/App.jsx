ad080b80927e65ffd6f955da186709fb16894c07
352ismailkhan@gmail.com <352ismailkhan@gmail.com>
2026-01-09 15:20:51 +0500

    create-Order-Tracking-Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { HomePage } from "./pages/HomePage";
 import { Routes, Route } from "react-router";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import "./App.css";
+import { TrackingPage } from "./pages/TrackingPage";
+import { OrderPage } from "./pages/OrderPage";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
+      <Route path="orders" element={<OrderPage />} />
     </Routes>
   );
 }
