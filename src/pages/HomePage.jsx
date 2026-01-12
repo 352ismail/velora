795009f88d059b90afe7b4c368266a0d09c67b67
352ismailkhan@gmail.com <352ismailkhan@gmail.com>
2026-01-12 12:22:19 +0500

    request-BE-for-products

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,11 @@ import "./HomePage.css";
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 export function HomePage() {
+  fetch("http://localhost:3000/api/products").then((product)=>{
+    return product.json();
+  }).then((data)=>{
+    console.log(data);
+  })
   return (
     <>
       <title>Velora E-commerce</title>
